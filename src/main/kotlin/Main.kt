050e8a71783940f5cc9eec82442becda1cb0622e
MINESORT <ilyamironenko1314@gmail.com>
2024-02-18 20:43:55 +0300

    Консольное приложение №2 СПРИНТ 7 (18.02.2024)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,5 @@
 fun main(args: Array<String>) {
-<<<<<<< HEAD
-    println("Hello World!")
-=======
     val launch = Menu()
     launch.mainMenu()
->>>>>>> 889f9e0 (Консольное приложение №2 СПРИНТ 7 (18.02.2024))
+
 }
